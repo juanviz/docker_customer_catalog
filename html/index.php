3c339140ae187d2746114f110363db273f7c3990
Juan Vicente Herrera <juaherre@redhat.com>
2025-02-04 22:43:36 +0100

    improved php web

--- html/index.php
+++ html/index.php
@@ -1,55 +1,81 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Tecnologías para la digitalización</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <style>
+    body {
+      background-color: #f8f9fa;
+    }
+    .jumbotron {
+      background: linear-gradient(135deg, #6a11cb, #2575fc);
+      color: white;
+      padding: 4rem;
+      border-radius: 15px;
+      text-align: center;
+    }
+    .table-container {
+      background: white;
+      padding: 2rem;
+      border-radius: 15px;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+  </style>
 </head>
 <body>
-  <div class = "container">
+  <div class="container mt-5">
     <div class="jumbotron">
-      <h1 class="display-4">Tecnologías de la digitalización 2024-2025</h1>
-      <p class="lead">Aplicacion PHP y MySQL con contenedores desplegada mediante Terraform</p>
+      <h1 class="display-4">Tecnologías de la Digitalización 2024-2025</h1>
+      <p class="lead">Aplicación PHP y MySQL con contenedores desplegada mediante Terraform</p>
       <hr class="my-4">
-      <p>Contenedor para Apache/PHP y otro para MySQL con almacenamiento de aplicación y de datos en volúmenes externos</p>
+      <p>Contenedor para Apache/PHP y otro para MySQL con almacenamiento de aplicación y de datos en volúmenes externos.</p>
     </div>
-    <table class="table table-striped table-responsive">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Credit Rating</th>
-          <th>Address</th>
-          <th>City</th>
-          <th>State</th>
-          <th>Country</th>
-          <th>Zip</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $conexion = new mysqli("mysql", "root", "secret", "SG");
+    
+    <div class="table-container mt-4">
+      <h3 class="mb-3"><i class="fas fa-database"></i> Lista de Clientes</h3>
+      <table class="table table-hover table-bordered">
+        <thead class="table-dark">
+          <tr>
+            <th>Nombre</th>
+            <th>Calificación Crediticia</th>
+            <th>Dirección</th>
+            <th>Ciudad</th>
+            <th>Estado</th>
+            <th>País</th>
+            <th>Código Postal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $conexion = new mysqli("mysql", "root", "secret", "SG");
 
-        $cadenaSQL = "select * from s_customer";
-        $resultado = $conexion->query($cadenaSQL);
+          if ($conexion->connect_error) {
+              die("Error de conexión: " . $conexion->connect_error);
+          }
 
-        while ($fila = $resultado->fetch_object()) {
-         echo "<tr><td> " .$fila->name . 
-         "</td><td>" . $fila->credit_rating .
-         "</td><td>" . $fila->address .
-         "</td><td>" . $fila->city .
-         "</td><td>" . $fila->state .
-         "</td><td>" . $fila->country .
-         "</td><td>" . $fila->zip_code .
-         "</td></tr>";
-       }
+          $cadenaSQL = "SELECT * FROM s_customer";
+          $resultado = $conexion->query($cadenaSQL);
 
-       ?>
-     </tbody>
-   </table>
- </div>
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+          while ($fila = $resultado->fetch_object()) {
+            echo "<tr>
+                    <td>{$fila->name}</td>
+                    <td>{$fila->credit_rating}</td>
+                    <td>{$fila->address}</td>
+                    <td>{$fila->city}</td>
+                    <td>{$fila->state}</td>
+                    <td>{$fila->country}</td>
+                    <td>{$fila->zip_code}</td>
+                  </tr>";
+          }
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
